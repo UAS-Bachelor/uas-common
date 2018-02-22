06364c9ee405f2566a34d00f6d80f84310ef2a8c
Lars Petri Christensen <larsc15@student.sdu.dk>
2018-02-22 17:31:11 +0100

    Further db impl tries - still not working

--- services/login/login.py
+++ services/login/login.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template, url_for, request
 import sys
 import argparse
+import time
 from os import system
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+"""
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql:////static/login.db'
+db = SQLAlchemy(app)
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=time.strftime('%Y-%m-%d %H:%M:%S'))
+
+# how to print the users in debug
+def __repr__(self):
+        return '<User {}>'.format(self.username) 
+"""
 @app.route('/login', methods=['GET','POST'])
 def index():
 #    if request.method == 'POST':
